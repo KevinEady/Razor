eee280a304d3b5a2e18720010f3c3364b828eb9f
Kevin Eady <kevin.c.eady@gmail.com>
2020-06-02 00:07:30 +0200

    yeah! on consecutive player move

--- Razor/ClearScript/Bindings.cs
+++ Razor/ClearScript/Bindings.cs
@@ -68,16 +68,42 @@ namespace Assistant.ClearScriptEngine
     {
       m_OLV = olv;
       m_Plugins = new List<Plugin>();
-      m_Engine = new V8ScriptEngine(V8ScriptEngineFlags.EnableDynamicModuleImports);
+      m_Engine = new V8ScriptEngine(V8ScriptEngineFlags.EnableDynamicModuleImports | V8ScriptEngineFlags.EnableDebugging, 9422);
       m_Cols = new OLVColumns();
       m_Cols.enabled = olv.GetColumn(0);
       m_Cols.name = olv.GetColumn(1);
       m_Cols.run = olv.GetColumn(2);
 
       m_Engine.DocumentSettings.AccessFlags = DocumentAccessFlags.EnableFileLoading;
-      m_Engine.DocumentSettings.AddSystemDocument("cuo", ModuleCategory.Standard, @"export class Plugin { }");
+      m_Engine.DocumentSettings.AddSystemDocument("cuo", ModuleCategory.Standard, @"export class Plugin { install() {} uninstall() {} }");
+      m_Engine.DocumentSettings.AddSystemDocument("util", ModuleCategory.Standard, @"
+export function sleep(duration) { 
+  return new Promise(resolve => Timer.DelayedCallback(TimeSpan.FromMilliseconds(duration), new TimerCallback(resolve)).Start());
+}
+export async function move(direction) {
+  try {
+    for (var i = 0; i < 8; i++) 
+    {
+      player.move(direction);
+      await sleep(400);
+    }
+  } catch (e) { }
+}
+
+globalThis.sleep2 = sleep;
+");
+
+      m_Engine.AddHostType("TimerCallback", typeof(TimerCallback));
+      m_Engine.AddHostType("Timer", typeof(Timer));
+      m_Engine.AddHostType("TimeSpan", typeof(TimeSpan));
+
+     // m_Engine.AddHostObject("host", new ExtendedHostFunctions());
+
+
+      m_Engine.AddHostObject("player", new ClearScriptBinding.Player(m_Engine));
 
       Recurse(Config.GetUserDirectory("Plugins"));
+
       InitializeObjectListView();
     }
 
@@ -158,7 +184,7 @@ namespace Assistant.ClearScriptEngine
       }
       else if (e.ColumnIndex == 2)
       {
-        e.SubItem.Text = "Run!!";
+        e.SubItem.Text = "Run";
       }
     }
 
@@ -175,7 +201,7 @@ namespace Assistant.ClearScriptEngine
         try
         {
           Plugin p = new Plugin(pluginPaths[0]);
-          p.Load(m_Engine);
+          p.Load();
           Add(p);
         }
         catch (Exception e)
@@ -201,6 +227,102 @@ namespace Assistant.ClearScriptEngine
       }
     }
   }
+
+  public class Plugin
+  {
+
+    private class Manifest
+    {
+      public string name { get; set; }
+      public string version { get; set; }
+      public string description { get; set; }
+      public string main { get; set; }
+    }
+
+    object m_Settings;
+    string m_ManifestPath;
+    Manifest m_Manifest;
+    V8Script m_Module;
+    dynamic m_PluginInst;
+    bool m_Enabled;
+
+    public string Name
+    {
+      get { return m_Manifest.name; }
+    }
+
+    public string Description
+    {
+      get { return m_Manifest.description; }
+    }
+
+    public string Run
+    {
+      get { return "Run"; }
+    }
+
+    public Plugin(string manifestPath)
+    {
+      m_ManifestPath = manifestPath;
+
+    }
+    public void Load()
+    {
+      using (StreamReader r = new StreamReader(m_ManifestPath))
+      {
+        string json = r.ReadToEnd();
+        m_Manifest = JsonConvert.DeserializeObject<Manifest>(json);
+
+        if (m_Manifest.main.StartsWith(".."))
+        {
+          throw new Exception("Plugin main must not start with ..");
+        }
+
+
+        //var def = m_Exports["default"];
+        //PluginManager.Log("The square is {0}", new def);
+
+      }
+    }
+    public void Install(V8ScriptEngine engine)
+    {
+      if (m_PluginInst == null)
+      {
+        string mainJs = Path.Combine(Path.GetDirectoryName(m_ManifestPath), m_Manifest.main);
+        string js = @"import plugin from '" + new Uri(mainJs).AbsoluteUri + "'; new plugin;";
+
+        // m_Module = engine.Compile(new DocumentInfo { Category = ModuleCategory.Standard }, js);
+        m_PluginInst = engine.Evaluate(new DocumentInfo { Category = ModuleCategory.Standard }, js);
+        m_PluginInst.install();
+      }
+
+    }
+
+    public void Uninstall()
+    {
+      if (m_PluginInst != null)
+      {
+        m_PluginInst.uninstall();
+      }
+    }
+
+    internal void Execute(V8ScriptEngine engine)
+    {
+      try
+      {
+        if (m_PluginInst == null)
+        {
+          Install(engine);
+        }
+        m_PluginInst.exec();
+      }
+      catch (Exception ex)
+      {
+        PluginManager.Log("Error executing plugin: {0}", ex.ToString());
+      }
+    }
+  }
+
   public class NamedDescriptionDecoration : BrightIdeasSoftware.AbstractDecoration
   {
     public ImageList ImageList;
@@ -257,69 +379,6 @@ namespace Assistant.ClearScriptEngine
     }
   }
 
-  public class Plugin
-  {
-
-    private class Manifest
-    {
-      public string name { get; set; }
-      public string version { get; set; }
-      public string description { get; set; }
-      public string main { get; set; }
-    }
-
-    object m_Settings;
-    string m_ManifestPath;
-    Manifest m_Manifest;
-    V8Script m_Module;
-    dynamic m_Exports;
-    bool m_Enabled;
-
-    public string Name
-    {
-      get { return m_Manifest.name; }
-    }
-
-    public string Description
-    {
-      get { return m_Manifest.description; }
-    }
-
-    public string Run
-    {
-      get { return "Run"; }
-    }
-
-    public Plugin(string manifestPath)
-    {
-      m_ManifestPath = manifestPath;
-
-    }
-    public void Load(V8ScriptEngine engine)
-    {
-      using (StreamReader r = new StreamReader(m_ManifestPath))
-      {
-        string json = r.ReadToEnd();
-        m_Manifest = JsonConvert.DeserializeObject<Manifest>(json);
-        string mainJs = Path.Combine(Path.GetDirectoryName(m_ManifestPath), m_Manifest.main);
-
-        string js = @"import * as plugin from '" + new Uri(mainJs).AbsoluteUri + "'; plugin;";
-        m_Module = engine.Compile(new DocumentInfo { Category = ModuleCategory.Standard }, js);
-
-        m_Exports = engine.Evaluate(m_Module);
-
-
-        PluginManager.Log("The square is {0}", m_Exports["default"](3));
-
-      }
-    }
-
-
-    internal void Execute(V8ScriptEngine engine)
-    {
-      // engine.
-    }
-  }
 }
 
 namespace Assistant.ClearScriptBinding
@@ -339,6 +398,34 @@ namespace Assistant.ClearScriptBinding
     {
       get { return World.Player?.Name; }
       set { }
+    }
+
+    public void overhead(string format, dynamic opts = null)
+    {
+      {
+        int hue = Config.GetInt("SysColor");
+        try
+        {
+          hue = opts.hue;
+        }
+        catch { /* ignore */ };
+
+        World.Player?.OverheadMessage(hue, format);
+      }
+    }
+
+    public void move(string direction, int times = 1)
+    {
+      times = Math.Max(Math.Min(times, 1), 10);
+      try
+      {
+        Direction dir = (Direction)Enum.Parse(typeof(Direction), direction, true);
+        for (int i = 0; i < times; i++)
+        {
+          Client.Instance.RequestMove(dir);
+
+        }
+      } catch { /* ignore */ }
     }
 
     public Item backpack
